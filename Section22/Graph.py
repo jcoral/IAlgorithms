39047b56e203ba659e0902f8c99ddcd396ccdfe3
jcoral <2811412910@qq.com>
2017-08-16 16:45:05 +0800

    Kruskal

--- Section22/Graph.py
+++ Section22/Graph.py
@@ -101,6 +101,9 @@ class Graph:
             self.addEdge(edge)
         graph.mapEdges(fn=unionEdge)
 
+    def sameEdgeUG(self, e1, e2):
+        return e1.srcVertice == e2.dstVertice and e1.dstVertice == e2.srcVertice
+
     def mapVertices(self, id = None, fn = None, filterFN = None):
         """
         遍历节点
@@ -129,20 +132,36 @@ class Graph:
 
         return vs
 
-    def mapEdges(self, id = None, fn = None, filterFN = None, needSorted=False):
+    def mapEdges(self, id = None, fn = None, needSorted=False, filterUGEdge=False, filterFN = None):
         """
         遍历边
         :param id: None：所有的边，id：与id节点相连的边
         :param fn: fn(edge) 对每一个边所要进行的操作
+        :param needSorted: 对边是否需要排序
+        :param filterUGEdge: 对无向图的边去重复
         :param filterFN: (edge) -> Bool 过滤边
         :return: 返回所有的边
         """
         if id is None: es = self.edges
         else: es = self.__graphDic__[id].values()
 
-        i = 0
         if needSorted:
             es.sort()
+
+        if filterUGEdge:
+            i = 0
+            while i < len(es):
+                j = i + 1
+                while j < len(es):
+                    sameEdge = self.sameEdgeUG(es[i], es[j])
+                    if sameEdge:
+                        es.pop(i)
+                        break
+                    j += 1
+                else:
+                    i += 1
+
+        i = 0
         while i < len(es):
             if filterFN is None or (filterFN is not None and filterFN(es[i])):
                 if fn is not None:
@@ -334,45 +353,68 @@ class Graph:
         return deepcopy(mst)
 
     def Kruskakl(self):
-        """最小生成树"""
+        """最小生成树
+
+        思想：
+        在所有的边中选取权重最小的边插入到子图中，选取完后从待选集合中删除掉
+        在插入的时候需要判断边是不是安全边，如果不是则舍弃
+
+        安全边：设A是某棵最小生成树的子集，选取的边Edge(u, v) 要满足A ∪ {Edge(u, v)}也是某棵最小生成树的子集
+        """
+
+        # 存放产生的子图
         treeSet = []
 
-        def findVertice(u, v):
+        def findVertice(e):
+            """
+            查找边的srcVertice和dstVertice属于哪一个子图
+            :param e: 边
+            :return: [g1, g2]
+            """
+            srcv = e.srcVertice
+            dstv = e.dstVertice
             uvPosition = [None, None]
-
             for tree in treeSet:
-                result = tree.mapVertices(filterFN=lambda v0: v0 == v or v0 == u)
+                result = tree.mapVertices(filterFN=lambda v0: v0 == dstv or v0 == srcv)
                 resultLength = len(result)
                 if resultLength == 1:
-                    if u == result[0]:
+                    if srcv == result[0]:
                         uvPosition[0] = tree
                     else:
                         uvPosition[1] = tree
                 elif resultLength == 2:
-                    if u == result[0] or u == result[1]:
-                        uvPosition[0] = tree
-                    else:
-                        uvPosition[1] = tree
+                    uvPosition[0] = tree
+                    uvPosition[1] = tree
 
-                if uvPosition[0] is not None and uvPosition[1] is not None: return
+                if uvPosition[0] is not None and uvPosition[1] is not None: return uvPosition
 
             return uvPosition
 
         def generateTree(edge):
             g = Graph()
             g.addEdge(edge)
+            g.addEdge(self.getEdge(edge.dstVertice, edge.srcVertice))
             treeSet.append(g)
 
         def addMSTEdge(edge):
-            srct, dstt = findVertice(edge.srcVertice, edge.dstVertice)
+            srct, dstt = findVertice(edge)
             targetTree = None
+
+            if srct == dstt and srct is not None: return
+
+            # 如果变没有在任何一棵树中，则生成一棵树
             if srct is None and dstt is None:
                 generateTree(edge)
                 return
-            if srct is None and dstt is not None:
-                targetTree = dstt
-            if srct is not None and dstt is None:
-                targetTree = srct
+
+            # 边的终点在dstt树中
+            if srct is None and dstt is not None: targetTree = dstt
+
+            # 边的源点在srct树中
+            if srct is not None and dstt is None: targetTree = srct
+
+            # 如果边的源点和终点不在同一棵树中
+            # 则合并两棵树
             if srct != dstt and srct is not None and dstt is not None:
                 srct.unionGraph(dstt)
                 treeSet.remove(dstt)
@@ -383,7 +425,7 @@ class Graph:
                 targetTree.addEdge(edge)
                 targetTree.addEdge(self.getEdge(edge.dstVertice, edge.srcVertice))
 
-        self.mapEdges(fn=addMSTEdge, needSorted=True)
+        self.mapEdges(fn=addMSTEdge, needSorted=True, filterUGEdge=True)
         self.__resetVerticesStatus__()
         return deepcopy(treeSet[0])
 
@@ -548,16 +590,6 @@ class Graph:
         graphDic = json.load(f, encoding="utf-8")
         f.close()
         return Graph.generateGraph(graphDic)
-
-    def choiceMinWeightVertice(self, srcv):
-        edges = self.mapEdges(id=srcv.id, filterFN=lambda e: e.__mark__ == 0)
-        if len(edges) == 0: return []
-        minWeightEdge = min(edges)
-        minWeightEdge.__mark__ = 1
-        reverseEdge = self.getEdge(minWeightEdge.dstVertice, minWeightEdge.srcVertice)
-        if reverseEdge is not None:
-            reverseEdge.__mark__ = 1
-        return [minWeightEdge.dstVertice]
 
 
 
